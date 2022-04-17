c0a23b081f28cbf4394d657074f95643d9dc4b20
Kuazk <allenz@M1-Mac-Allen.local>
2022-04-16 21:36:09 -0500

    frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from "react-tsparticles";
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -52,33 +53,9 @@ const particleOptions = {
         },
       },
 },}
-const raw = JSON.stringify({
-  "user_app_id": {
-        "user_id": "xqrl2mku7xik",
-        "app_id": "646374474826499e92008216527d3e06"
-    },
-  "inputs": [
-    {
-      "data": {
-        "image": {
-          "url": "https://samples.clarifai.com/metro-north.jpg"
-        }
-      }
-    }
-  ]
-});
 
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Authorization': 'Key 5f6a999ee5ad4c7093bc877c3a56674e'
-  },
-  body: raw
-};
-const app = new Clarifai.App({
-  apiKey: '5f6a999ee5ad4c7093bc877c3a56674e'
- });
+
+
 
 // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
 // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
@@ -91,17 +68,73 @@ class App extends React.Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
+      box: {},
     }
   };
+
+  calculateFaceLocation =(result) => {
+
+    const clarifaiFace = JSON.parse(result, null, 2).outputs[0].data.regions[0].region_info.bounding_box;
+    
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    
+   
+
+    return {
+      leftCol : clarifaiFace.left_col * width,
+      topRow : clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col *width),
+      bottomRow: height - (clarifaiFace.bottom_row *height)
+    }
+
+  }
+  
+  displayFaceBox = (box) =>{
+    
+    this.setState({box});
+  
+  }
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
+    
   }
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: this.state.input});
+  
+    const raw = JSON.stringify({
+      "user_app_id": {
+            "user_id": "xqrl2mku7xik",
+            "app_id": "646374474826499e92008216527d3e06"
+        },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": this.state.input
+            }
+          }
+        }
+      ]
+    });
+    
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key 5f6a999ee5ad4c7093bc877c3a56674e'
+      },
+      body: raw
+    };
+    
+
     fetch(`https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs`, requestOptions)
     .then(response => response.text())
-    .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data))
+    .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
     .catch(error => console.log('error', error));
+
   }
   render() {
     return (
@@ -113,12 +146,15 @@ class App extends React.Component {
         <Navigation />
         <Logo />
         <Rank />
+        
+
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
 
           />
-          <FaceRecognition />
+        <FaceRecognition box = {this.state.box} imageUrl={this.state.imageUrl}/>
+        
       </div>
     );
   }
